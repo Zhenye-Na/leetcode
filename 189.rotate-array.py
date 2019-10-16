31909f39867a7f78c6936e367ec4dd94e180bd63
Zhenye Na <zhenye.na@nuance.com>
2019-10-16 12:08:01 -0700

    189.rotate-array.py

---
+++ 189.rotate-array.py
@@ -0,0 +1,63 @@
+#
+# @lc app=leetcode id=189 lang=python3
+#
+# [189] Rotate Array
+#
+# https://leetcode.com/problems/rotate-array/description/
+#
+# algorithms
+# Easy (31.78%)
+# Likes:    1712
+# Dislikes: 669
+# Total Accepted:    356.4K
+# Total Submissions: 1.1M
+# Testcase Example:  '[1,2,3,4,5,6,7]\n3'
+#
+# Given an array, rotate the array to the right by k steps, where k is
+# non-negative.
+# 
+# Example 1:
+# 
+# 
+# Input: [1,2,3,4,5,6,7] and k = 3
+# Output: [5,6,7,1,2,3,4]
+# Explanation:
+# rotate 1 steps to the right: [7,1,2,3,4,5,6]
+# rotate 2 steps to the right: [6,7,1,2,3,4,5]
+# rotate 3 steps to the right: [5,6,7,1,2,3,4]
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: [-1,-100,3,99] and k = 2
+# Output: [3,99,-1,-100]
+# Explanation: 
+# rotate 1 steps to the right: [99,-1,-100,3]
+# rotate 2 steps to the right: [3,99,-1,-100]
+# 
+# 
+# Note:
+# 
+# 
+# Try to come up as many solutions as you can, there are at least 3 different
+# ways to solve this problem.
+# Could you do it in-place with O(1) extra space?
+# 
+#
+
+# @lc code=start
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if not nums or len(nums) == 0 or k < 0:
+            return nums
+
+        k = k % len(nums)
+
+        nums[: len(nums) - k] = list(reversed(nums[: len(nums) - k]))
+        nums[len(nums) - k :] = list(reversed(nums[len(nums) - k :]))
+        nums[:] = list(reversed(nums))
+# @lc code=end
