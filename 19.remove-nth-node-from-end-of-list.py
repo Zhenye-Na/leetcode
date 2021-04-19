162dc0621aa859982fdd48ec21f361d35d8f87cf
Na, Zhenye <nazhenye@gmail.com>
2021-04-18 22:25:48 -0700

    leetcode 041821

---
+++ 19.remove-nth-node-from-end-of-list.py
@@ -0,0 +1,78 @@
+#
+# @lc app=leetcode id=19 lang=python3
+#
+# [19] Remove Nth Node From End of List
+#
+# https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+#
+# algorithms
+# Medium (35.65%)
+# Likes:    5231
+# Dislikes: 302
+# Total Accepted:    852.5K
+# Total Submissions: 2.4M
+# Testcase Example:  '[1,2,3,4,5]\n2'
+#
+# Given the head of a linked list, remove the n^th node from the end of the
+# list and return its head.
+# 
+# Follow up: Could you do this in one pass?
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: head = [1,2,3,4,5], n = 2
+# Output: [1,2,3,5]
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: head = [1], n = 1
+# Output: []
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: head = [1,2], n = 1
+# Output: [1]
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# The number of nodes in the list is sz.
+# 1 <= sz <= 30
+# 0 <= Node.val <= 100
+# 1 <= n <= sz
+# 
+# 
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        prev, curr, fast = None, head, head
+        for _ in range(n - 1):
+            fast = fast.next
+
+        while fast.next is not None:
+            prev = curr
+            curr = curr.next
+            fast = fast.next
+
+        if prev:
+            prev.next = curr.next
+        else:
+            head = curr.next
+        return head
+# @lc code=end
+
