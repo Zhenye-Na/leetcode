c356d5d5aec1af2523e1d43cc4cc8598b0d34606
Na, Zhenye <nazhenye@gmail.com>
2021-07-16 23:52:15 -0700

    leetcode 071621
    18.4-sum

---
+++ java/18.4-sum.java
@@ -0,0 +1,100 @@
+/*
+ * @lc app=leetcode id=18 lang=java
+ *
+ * [18] 4Sum
+ *
+ * https://leetcode.com/problems/4sum/description/
+ *
+ * algorithms
+ * Medium (35.68%)
+ * Likes:    3937
+ * Dislikes: 468
+ * Total Accepted:    454.7K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[1,0,-1,0,-2,2]\n0'
+ *
+ * Given an array nums of n integers, return an array of all the unique
+ * quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+ * 
+ * 
+ * 0 <= a, b, c, d < n
+ * a, b, c, and d are distinct.
+ * nums[a] + nums[b] + nums[c] + nums[d] == target
+ * 
+ * 
+ * You may return the answer in any order.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [1,0,-1,0,-2,2], target = 0
+ * Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [2,2,2,2,2], target = 8
+ * Output: [[2,2,2,2]]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= nums.length <= 200
+ * -10^9 <= nums[i] <= 10^9
+ * -10^9 <= target <= 10^9
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> rst = new ArrayList<List<Integer>>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 3; i++) {
+            if (i != 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                if (j != i + 1 && nums[j] == nums[j - 1])
+                    continue;
+
+                int left = j + 1;
+                int right = nums.length - 1;
+                while (left < right) {
+                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum < target) {
+                        left++;
+                    } else if (sum > target) {
+                        right--;
+                    } else {
+                        ArrayList<Integer> tmp = new ArrayList<Integer>();
+                        tmp.add(nums[i]);
+                        tmp.add(nums[j]);
+                        tmp.add(nums[left]);
+                        tmp.add(nums[right]);
+                        rst.add(tmp);
+                        left++;
+                        right--;
+                        while (left < right && nums[left] == nums[left - 1]) {
+                            left++;
+                        }
+                        while (left < right && nums[right] == nums[right + 1]) {
+                            right--;
+                        }
+                    }
+                }
+            }
+        }
+
+        return rst;
+    }
+}
+// @lc code=end
+
---
+++ two-pointers.md
@@ -0,0 +1,5 @@
+---
+layout: archive
+which_category: two-pointers
+title: All posts of category 'two-pointers'
+---
