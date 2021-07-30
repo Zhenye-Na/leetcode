8e3974b1d5fb65f4eed494b5a65c9b4672524152
Na, Zhenye <nazhenye@gmail.com>
2021-07-30 00:27:22 -0700

    leetcode 072921
    542.01-matrix

--- python/542.01-matrix.py
+++ python/542.01-matrix.py
@@ -13,7 +13,7 @@
 # Total Submissions: 362.3K
 # Testcase Example:  '[[0,0,0],[0,1,0],[0,0,0]]'
 #
-# Given an m x n binary matrix mat, return the distance of the nearest 0 for
+# Given an m x n binary matrix matrix, return the distance of the nearest 0 for
 # each cell.
 # 
 # The distance between two adjacent cells is 1.
@@ -22,14 +22,14 @@
 # Example 1:
 # 
 # 
-# Input: mat = [[0,0,0],[0,1,0],[0,0,0]]
+# Input: matrix = [[0,0,0],[0,1,0],[0,0,0]]
 # Output: [[0,0,0],[0,1,0],[0,0,0]]
 # 
 # 
 # Example 2:
 # 
 # 
-# Input: mat = [[0,0,0],[0,1,0],[1,1,1]]
+# Input: matrix = [[0,0,0],[0,1,0],[1,1,1]]
 # Output: [[0,0,0],[0,1,0],[1,2,1]]
 # 
 # 
@@ -37,19 +37,58 @@
 # Constraints:
 # 
 # 
-# m == mat.length
-# n == mat[i].length
+# m == matrix.length
+# n == matrix[i].length
 # 1 <= m, n <= 10^4
 # 1 <= m * n <= 10^4
-# mat[i][j] is either 0 or 1.
-# There is at least one 0 in mat.
+# matrix[i][j] is either 0 or 1.
+# There is at least one 0 in matrix.
 # 
 # 
 #
 
 # @lc code=start
+import sys
+
+from collections import deque
+
 class Solution:
-    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+
+    def __init__(self):
+        self.dx = [1, -1, 0, 0]
+        self.dy = [0, 0, 1, -1]
+    
+    def updateMatrix(self, matrix: List[List[int]]) -> List[List[int]]:
+
+        if not matrix or len(matrix) == 0 or len(matrix[0]) == 0:
+            return matrix
+
+        n, m = len(matrix), len(matrix[0])
+        dist = [[0 for _ in range(m)] for _ in range(n)]
+        queue = deque([])
+
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j] == 0:
+                    dist[i][j] = 0
+                    queue.append([i, j])
+                else:
+                    dist[i][j] = sys.maxsize
         
+        while queue:
+            x, y = queue.popleft()
+            for d in range(4):
+                next_x, next_y = x + self.dx[d], y + self.dy[d]
+                if self.isValid(next_x, next_y, matrix):
+                    if (dist[next_x][next_y] > dist[x][y] + 1):
+                        dist[next_x][next_y] = dist[x][y] + 1
+                        queue.append([next_x, next_y])
+
+        return dist
+
+
+    def isValid(self, x, y, matrix):
+        return 0 <= x < len(matrix) and 0 <= y < len(matrix[0])
+
 # @lc code=end
 
