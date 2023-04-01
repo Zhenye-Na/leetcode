18d91a6ba813f91531b04632563212dfde2cceb9
Na, Zhenye <32248549+Zhenye-Na@users.noreply.github.com>
2023-03-31 17:18:44 -0700

    leetcode 033123
    binary search

---
+++ python/704.binary-search.py
@@ -0,0 +1,75 @@
+#
+# @lc app=leetcode id=704 lang=python3
+#
+# [704] Binary Search
+#
+# https://leetcode.com/problems/binary-search/description/
+#
+# algorithms
+# Easy (54.83%)
+# Likes:    8499
+# Dislikes: 183
+# Total Accepted:    1.6M
+# Total Submissions: 3M
+# Testcase Example:  '[-1,0,3,5,9,12]\n9'
+#
+# Given an array of integers nums which is sorted in ascending order, and an
+# integer target, write a function to search target in nums. If target exists,
+# then return its index. Otherwise, return -1.
+# 
+# You must write an algorithm with O(log n) runtime complexity.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: nums = [-1,0,3,5,9,12], target = 9
+# Output: 4
+# Explanation: 9 exists in nums and its index is 4
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: nums = [-1,0,3,5,9,12], target = 2
+# Output: -1
+# Explanation: 2 does not exist in nums so return -1
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= nums.length <= 10^4
+# -10^4 < nums[i], target < 10^4
+# All the integers in nums are unique.
+# nums is sorted in ascending order.
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if not nums or len(nums) == 0:
+            return -1
+
+        left, right = 0, len(nums) - 1
+        while left < right - 1:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid
+            else:
+                left = mid
+
+        if nums[right] == target:
+            return right
+        if nums[left] == target:
+            return left
+
+        return -1
+
+# @lc code=end
+
