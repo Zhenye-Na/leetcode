c0412518d64e85f8065fdc08c50549739e24c6d5
Na, Zhenye <32248549+Zhenye-Na@users.noreply.github.com>
2024-08-28 16:41:51 -0700

    greedy | reverse thinking

--- python/435.non-overlapping-intervals.py
+++ python/435.non-overlapping-intervals.py
@@ -3,19 +3,79 @@
 #
 # [435] Non-overlapping Intervals
 #
+# https://leetcode.com/problems/non-overlapping-intervals/description/
+#
+# algorithms
+# Medium (52.24%)
+# Likes:    8212
+# Dislikes: 224
+# Total Accepted:    624.7K
+# Total Submissions: 1.2M
+# Testcase Example:  '[[1,2],[2,3],[3,4],[1,3]]'
+#
+# Given an array of intervals intervals where intervals[i] = [starti, endi],
+# return the minimum number of intervals you need to remove to make the rest of
+# the intervals non-overlapping.
+#
+#
+# Example 1:
+#
+#
+# Input: intervals = [[1,2],[2,3],[3,4],[1,3]]
+# Output: 1
+# Explanation: [1,3] can be removed and the rest of the intervals are
+# non-overlapping.
+#
+#
+# Example 2:
+#
+#
+# Input: intervals = [[1,2],[1,2],[1,2]]
+# Output: 2
+# Explanation: You need to remove two [1,2] to make the rest of the intervals
+# non-overlapping.
+#
+#
+# Example 3:
+#
+#
+# Input: intervals = [[1,2],[2,3]]
+# Output: 0
+# Explanation: You don't need to remove any of the intervals since they're
+# already non-overlapping.
+#
+#
+#
+# Constraints:
+#
+#
+# 1 <= intervals.length <= 10^5
+# intervals[i].length == 2
+# -5 * 10^4 <= starti < endi <= 5 * 10^4
+#
+#
+#
+
+
+# @lc code=start
+from typing import List
+
+
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
-        if not intervals:
+        if not intervals or len(intervals) == 0:
             return 0
-            
+
         intervals = sorted(intervals, key=lambda x: x[1])
-        self.index = 0
-        self.result = 1
-        
+        prev_i = 0
+        selected = 1
+
         for i in range(1, len(intervals)):
-            if intervals[i][0] >= intervals[self.index][1]:
-                self.result += 1
-                self.index = i
+            if intervals[i][0] >= intervals[prev_i][1]:
+                selected += 1
+                prev_i = i
 
-        return len(intervals) - self.result
+        return len(intervals) - selected
 
+
+# @lc code=end
