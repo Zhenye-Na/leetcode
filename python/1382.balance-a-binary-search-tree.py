bed21f690a534ba97d53fd2192753f04c77852c6
Na, Zhenye <32248549+Zhenye-Na@users.noreply.github.com>
2024-09-12 22:23:15 -0700

    divide conquer | dfs

--- python/1382.balance-a-binary-search-tree.py
+++ python/1382.balance-a-binary-search-tree.py
@@ -16,36 +16,37 @@
 # Given the root of a binary search tree, return a balanced binary search tree
 # with the same node values. If there is more than one answer, return any of
 # them.
-# 
+#
 # A binary search tree is balanced if the depth of the two subtrees of every
 # node never differs by more than 1.
-# 
-# 
+#
+#
 # Example 1:
-# 
-# 
+#
+#
 # Input: root = [1,null,2,null,3,null,4,null,null]
 # Output: [2,1,3,null,null,null,4]
 # Explanation: This is not the only correct answer, [3,1,4,null,2] is also
 # correct.
-# 
-# 
+#
+#
 # Example 2:
-# 
-# 
+#
+#
 # Input: root = [2,1,3]
 # Output: [2,1,3]
-# 
-# 
-# 
+#
+#
+#
 # Constraints:
-# 
-# 
+#
+#
 # The number of nodes in the tree is in the range [1, 10^4].
 # 1 <= Node.val <= 10^5
-# 
-# 
 #
+#
+#
+
 
 # @lc code=start
 # Definition for a binary tree node.
@@ -63,7 +64,11 @@ class Solution:
         if root is None:
             return []
 
-        return self._inorder_traversal(root.left) + [root.val] + self._inorder_traversal(root.right)
+        return (
+            self._inorder_traversal(root.left)
+            + [root.val]
+            + self._inorder_traversal(root.right)
+        )
 
     def _reconstruct_binary_search_tree(self, nodes):
         if len(nodes) <= 0:
@@ -76,8 +81,9 @@ class Solution:
 
         root = TreeNode(nodes[mid])
         root.left = self._reconstruct_binary_search_tree(nodes[0:mid])
-        root.right = self._reconstruct_binary_search_tree(nodes[mid + 1:])
+        root.right = self._reconstruct_binary_search_tree(nodes[mid + 1 :])
 
         return root
-# @lc code=end
 
+
+# @lc code=end
