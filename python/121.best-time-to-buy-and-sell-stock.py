299204abd67ff0b7483bc6b1a613dcc5a9786e52
Na, Zhenye <32248549+Zhenye-Na@users.noreply.github.com>
2024-09-12 22:32:04 -0700

    array | pointers

--- python/121.best-time-to-buy-and-sell-stock.py
+++ python/121.best-time-to-buy-and-sell-stock.py
@@ -15,45 +15,59 @@
 #
 # You are given an array prices where prices[i] is the price of a given stock
 # on the i^th day.
-# 
+#
 # You want to maximize your profit by choosing a single day to buy one stock
 # and choosing a different day in the future to sell that stock.
-# 
+#
 # Return the maximum profit you can achieve from this transaction. If you
 # cannot achieve any profit, return 0.
-# 
-# 
+#
+#
 # Example 1:
-# 
-# 
+#
+#
 # Input: prices = [7,1,5,3,6,4]
 # Output: 5
 # Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit =
 # 6-1 = 5.
 # Note that buying on day 2 and selling on day 1 is not allowed because you
 # must buy before you sell.
-# 
-# 
+#
+#
 # Example 2:
-# 
-# 
+#
+#
 # Input: prices = [7,6,4,3,1]
 # Output: 0
 # Explanation: In this case, no transactions are done and the max profit =
 # 0.
-# 
-# 
-# 
+#
+#
+#
 # Constraints:
-# 
-# 
+#
+#
 # 1 <= prices.length <= 10^5
 # 0 <= prices[i] <= 10^4
-# 
-# 
+#
+#
 #
 
+
 # @lc code=start
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        min_val, max_val = max(prices), min(prices)
+        max_profit = 0
+        for i in range(len(prices)):
+            min_val = min(min_val, prices[i])
+            max_val = prices[i]
+            max_profit = max(max_profit, max_val - min_val)
+
+        return max_profit
+
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         if not prices or len(prices) == 0:
@@ -66,5 +80,5 @@ class Solution:
 
         return max_profit
 
-# @lc code=end
 
+# @lc code=end
