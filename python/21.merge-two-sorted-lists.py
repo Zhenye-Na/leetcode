9d91f723fa10f1c7c830a5cabc0ec984d24cd739
Na, Zhenye <32248549+Zhenye-Na@users.noreply.github.com>
2024-09-12 22:25:30 -0700

    linked list | sorting

--- python/21.merge-two-sorted-lists.py
+++ python/21.merge-two-sorted-lists.py
@@ -14,44 +14,45 @@
 # Testcase Example:  '[1,2,4]\n[1,3,4]'
 #
 # You are given the heads of two sorted linked lists list1 and list2.
-# 
+#
 # Merge the two lists in a one sorted list. The list should be made by splicing
 # together the nodes of the first two lists.
-# 
+#
 # Return the head of the merged linked list.
-# 
-# 
+#
+#
 # Example 1:
-# 
-# 
+#
+#
 # Input: list1 = [1,2,4], list2 = [1,3,4]
 # Output: [1,1,2,3,4,4]
-# 
-# 
+#
+#
 # Example 2:
-# 
-# 
+#
+#
 # Input: list1 = [], list2 = []
 # Output: []
-# 
-# 
+#
+#
 # Example 3:
-# 
-# 
+#
+#
 # Input: list1 = [], list2 = [0]
 # Output: [0]
-# 
-# 
-# 
+#
+#
+#
 # Constraints:
-# 
-# 
+#
+#
 # The number of nodes in both lists is in the range [0, 50].
 # -100 <= Node.val <= 100
 # Both list1 and list2 are sorted in non-decreasing order.
-# 
-# 
 #
+#
+#
+
 
 # @lc code=start
 # Definition for singly-linked list.
@@ -60,7 +61,9 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    def mergeTwoLists(
+        self, list1: Optional[ListNode], list2: Optional[ListNode]
+    ) -> Optional[ListNode]:
         if not list1 and not list2:
             return list1
 
@@ -89,5 +92,6 @@ class Solution:
             head.next = head2
 
         return dummy.next
-# @lc code=end
 
+
+# @lc code=end
