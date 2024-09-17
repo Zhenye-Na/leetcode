cdf3bd54a4dd8ad0adfaf21de722d1be1dae4635
Na, Zhenye <32248549+Zhenye-Na@users.noreply.github.com>
2024-09-16 20:36:49 -0700

    dfs | divide & conquer | binary tree

--- python/110.balanced-binary-tree.py
+++ python/110.balanced-binary-tree.py
@@ -14,45 +14,46 @@
 # Testcase Example:  '[3,9,20,null,null,15,7]'
 #
 # Given a binary tree, determine if it is height-balanced.
-# 
+#
 # For this problem, a height-balanced binary tree is defined as:
-# 
-# 
+#
+#
 # a binary tree in which the left and right subtrees of every node differ in
 # height by no more than 1.
-# 
-# 
-# 
+#
+#
+#
 # Example 1:
-# 
-# 
+#
+#
 # Input: root = [3,9,20,null,null,15,7]
 # Output: true
-# 
-# 
+#
+#
 # Example 2:
-# 
-# 
+#
+#
 # Input: root = [1,2,2,3,3,null,null,4,4]
 # Output: false
-# 
-# 
+#
+#
 # Example 3:
-# 
-# 
+#
+#
 # Input: root = []
 # Output: true
-# 
-# 
-# 
+#
+#
+#
 # Constraints:
-# 
-# 
+#
+#
 # The number of nodes in the tree is in the range [0, 5000].
 # -10^4 <= Node.val <= 10^4
-# 
-# 
 #
+#
+#
+
 
 # @lc code=start
 # Definition for a binary tree node.
@@ -65,7 +66,6 @@ class Solution:
     def isBalanced(self, root: TreeNode) -> bool:
         is_balance, _ = self._check(root)
         return is_balance
-
 
     def _check(self, root):
         """
@@ -94,5 +94,6 @@ class Solution:
             return False, 0
 
         return True, max(left_height, right_height) + 1
-# @lc code=end
 
+
+# @lc code=end
--- python/226.invert-binary-tree.py
+++ python/226.invert-binary-tree.py
@@ -14,38 +14,39 @@
 # Testcase Example:  '[4,2,7,1,3,6,9]'
 #
 # Given the root of a binary tree, invert the tree, and return its root.
-# 
-# 
+#
+#
 # Example 1:
-# 
-# 
+#
+#
 # Input: root = [4,2,7,1,3,6,9]
 # Output: [4,7,2,9,6,3,1]
-# 
-# 
+#
+#
 # Example 2:
-# 
-# 
+#
+#
 # Input: root = [2,1,3]
 # Output: [2,3,1]
-# 
-# 
+#
+#
 # Example 3:
-# 
-# 
+#
+#
 # Input: root = []
 # Output: []
-# 
-# 
-# 
+#
+#
+#
 # Constraints:
-# 
-# 
+#
+#
 # The number of nodes in the tree is in the range [0, 100].
 # -100 <= Node.val <= 100
-# 
-# 
 #
+#
+#
+
 
 # @lc code=start
 # Definition for a binary tree node.
@@ -54,6 +55,9 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from typing import Optional
+
+
 class Solution:
     def invertTree(self, root: TreeNode) -> TreeNode:
         if not root:
@@ -66,5 +70,21 @@ class Solution:
         root.left = right
 
         return root
-# @lc code=end
 
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        return self._dfs(root)
+
+    def _dfs(self, root):
+        if not root:
+            return None
+
+        left = self._dfs(root.left)
+        right = self._dfs(root.right)
+        root.left, root.right = right, left
+
+        return root
+
+
+# @lc code=end
