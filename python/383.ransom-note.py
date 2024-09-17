1c5d215080c4d4fdfade30b763d5f77f3ad36e10
Na, Zhenye <32248549+Zhenye-Na@users.noreply.github.com>
2024-09-16 20:45:47 -0700

    hashmap

---
+++ python/383.ransom-note.py
@@ -0,0 +1,58 @@
+#
+# @lc app=leetcode id=383 lang=python3
+#
+# [383] Ransom Note
+#
+# https://leetcode.com/problems/ransom-note/description/
+#
+# algorithms
+# Easy (59.54%)
+# Likes:    5054
+# Dislikes: 506
+# Total Accepted:    1.3M
+# Total Submissions: 2.1M
+# Testcase Example:  '"a"\n"b"'
+#
+# Given two strings ransomNote and magazine, return true if ransomNote can be
+# constructed by using the letters from magazine and false otherwise.
+#
+# Each letter in magazine can only be used once in ransomNote.
+#
+#
+# Example 1:
+# Input: ransomNote = "a", magazine = "b"
+# Output: false
+# Example 2:
+# Input: ransomNote = "aa", magazine = "ab"
+# Output: false
+# Example 3:
+# Input: ransomNote = "aa", magazine = "aab"
+# Output: true
+#
+#
+# Constraints:
+#
+#
+# 1 <= ransomNote.length, magazine.length <= 10^5
+# ransomNote and magazine consist of lowercase English letters.
+#
+#
+#
+
+# @lc code=start
+from collections import Counter
+
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ransom_counter = Counter(ransomNote)
+        magazine_counter = Counter(magazine)
+
+        for letter, count in ransom_counter.items():
+            if magazine_counter[letter] < count:
+                return False
+
+        return True
+
+
+# @lc code=end
