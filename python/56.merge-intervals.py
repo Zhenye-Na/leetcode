c9d7b5ac6889668254c475043db925d3ece02ecd
Na, Zhenye <32248549+Zhenye-Na@users.noreply.github.com>
2024-09-18 14:46:49 -0700

    intervals

--- python/56.merge-intervals.py
+++ python/56.merge-intervals.py
@@ -16,37 +16,41 @@
 # Given an array of intervals where intervals[i] = [starti, endi], merge all
 # overlapping intervals, and return an array of the non-overlapping intervals
 # that cover all the intervals in the input.
-# 
-# 
+#
+#
 # Example 1:
-# 
-# 
+#
+#
 # Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
 # Output: [[1,6],[8,10],[15,18]]
 # Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into
 # [1,6].
-# 
-# 
+#
+#
 # Example 2:
-# 
-# 
+#
+#
 # Input: intervals = [[1,4],[4,5]]
 # Output: [[1,5]]
 # Explanation: Intervals [1,4] and [4,5] are considered overlapping.
-# 
-# 
-# 
+#
+#
+#
 # Constraints:
-# 
-# 
+#
+#
 # 1 <= intervals.length <= 10^4
 # intervals[i].length == 2
 # 0 <= starti <= endi <= 10^4
-# 
-# 
+#
+#
 #
 
+
 # @lc code=start
+from typing import List
+
+
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         if not intervals or len(intervals) == 0:
@@ -61,20 +65,20 @@ class Solution:
 
         return res
 
-
     def merge2(self, intervals: List[List[int]]) -> List[List[int]]:
         if not intervals or len(intervals) == 0:
             return intervals
-        
+
         intervals.sort(key=lambda x: (x[0], x[1]))
-        
+
         merged = []
         for interval in intervals:
             if not merged or merged[-1][-1] < interval[0]:
                 merged.append(interval)
             else:
                 merged[-1][-1] = max(merged[-1][-1], interval[-1])
-                
-        return merged
-# @lc code=end
 
+        return merged
+
+
+# @lc code=end
--- python/57.insert-interval.py
+++ python/57.insert-interval.py
@@ -15,64 +15,70 @@
 #
 # Given a set of non-overlapping intervals, insert a new interval into the
 # intervals (merge if necessary).
-# 
+#
 # You may assume that the intervals were initially sorted according to their
 # start times.
-# 
-# 
+#
+#
 # Example 1:
-# 
-# 
+#
+#
 # Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
 # Output: [[1,5],[6,9]]
-# 
-# 
+#
+#
 # Example 2:
-# 
-# 
+#
+#
 # Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
 # Output: [[1,2],[3,10],[12,16]]
 # Explanation: Because the new interval [4,8] overlaps with
 # [3,5],[6,7],[8,10].
-# 
+#
 # Example 3:
-# 
-# 
+#
+#
 # Input: intervals = [], newInterval = [5,7]
 # Output: [[5,7]]
-# 
-# 
+#
+#
 # Example 4:
-# 
-# 
+#
+#
 # Input: intervals = [[1,5]], newInterval = [2,3]
 # Output: [[1,5]]
-# 
-# 
+#
+#
 # Example 5:
-# 
-# 
+#
+#
 # Input: intervals = [[1,5]], newInterval = [2,7]
 # Output: [[1,7]]
-# 
-# 
-# 
+#
+#
+#
 # Constraints:
-# 
-# 
+#
+#
 # 0 <= intervals.length <= 10^4
 # intervals[i].length == 2
 # 0 <= intervals[i][0] <= intervals[i][1] <= 10^5
 # intervals is sorted by intervals[i][0] in ascending order.
 # newInterval.length == 2
 # 0 <= newInterval[0] <= newInterval[1] <= 10^5
-# 
-# 
+#
+#
 #
 
+
 # @lc code=start
+from typing import List
+
+
 class Solution:
-    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+    def insert(
+        self, intervals: List[List[int]], newInterval: List[int]
+    ) -> List[List[int]]:
         intervals.append(newInterval)
         return self._merge(intervals)
 
@@ -88,5 +94,6 @@ class Solution:
             res[-1][1] = max(res[-1][1], end)
 
         return res
-# @lc code=end
 
+
+# @lc code=end
