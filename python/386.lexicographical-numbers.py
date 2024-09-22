0c653b05eb81604322f0e847dcb889d5edd83e90
Na, Zhenye <32248549+Zhenye-Na@users.noreply.github.com>
2024-09-21 22:52:19 -0700

    dfs
    leetcode 092124

--- python/386.lexicographical-numbers.py
+++ python/386.lexicographical-numbers.py
@@ -15,35 +15,39 @@
 #
 # Given an integer n, return all the numbers in the range [1, n] sorted in
 # lexicographical order.
-# 
-# 
+#
+#
 # Example 1:
 # Input: n = 13
 # Output: [1,10,11,12,13,2,3,4,5,6,7,8,9]
 # Example 2:
 # Input: n = 2
 # Output: [1,2]
-# 
-# 
+#
+#
 # Constraints:
-# 
-# 
+#
+#
 # 1 <= n <= 5 * 10^4
-# 
-# 
-# 
+#
+#
+#
 # Follow up: Could you optimize your solution to use O(n) runtime and O(1)
 # space?
-# 
+#
 #
 
+
 # @lc code=start
+from typing import List
+
+
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
         res = []
         curr_num = 1
 
-        for i in range(n):
+        for _ in range(n):
             res.append(curr_num)
 
             if curr_num * 10 <= n:
@@ -57,5 +61,32 @@ class Solution:
                     curr_num = curr_num // 10
 
         return res
-# @lc code=end
 
+
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        res = []
+        for i in range(1, 10):
+            self._dfs(i, n, res)
+
+        return res
+
+    def _dfs(self, curr_num: int, n: int, res: List[int]) -> None:
+        if curr_num > n:
+            return
+
+        res.append(curr_num)
+        for i in range(10):
+            next_num = curr_num * 10 + i
+            if next_num > n:
+                break
+            self._dfs(next_num, n, res)
+
+
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        res = [i for i in range(1, n + 1)]
+        return sorted(res, key=lambda x: str(x))
+
+
+# @lc code=end
