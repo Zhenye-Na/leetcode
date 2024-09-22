c7e306e0ffdd456dfb920759ac7b393ffc13194f
Na, Zhenye <32248549+Zhenye-Na@users.noreply.github.com>
2024-09-21 22:25:47 -0700

    bfs

--- python/542.01-matrix.py
+++ python/542.01-matrix.py
@@ -15,36 +15,36 @@
 #
 # Given an m x n binary matrix matrix, return the distance of the nearest 0 for
 # each cell.
-# 
+#
 # The distance between two adjacent cells is 1.
-# 
-# 
+#
+#
 # Example 1:
-# 
-# 
+#
+#
 # Input: matrix = [[0,0,0],[0,1,0],[0,0,0]]
 # Output: [[0,0,0],[0,1,0],[0,0,0]]
-# 
-# 
+#
+#
 # Example 2:
-# 
-# 
+#
+#
 # Input: matrix = [[0,0,0],[0,1,0],[1,1,1]]
 # Output: [[0,0,0],[0,1,0],[1,2,1]]
-# 
-# 
-# 
+#
+#
+#
 # Constraints:
-# 
-# 
+#
+#
 # m == matrix.length
 # n == matrix[i].length
 # 1 <= m, n <= 10^4
 # 1 <= m * n <= 10^4
 # matrix[i][j] is either 0 or 1.
 # There is at least one 0 in matrix.
-# 
-# 
+#
+#
 #
 
 # @lc code=start
@@ -52,12 +52,13 @@ import sys
 
 from collections import deque
 
+
 class Solution:
 
     def __init__(self):
         self.dx = [1, -1, 0, 0]
         self.dy = [0, 0, 1, -1]
-    
+
     def updateMatrix(self, matrix: List[List[int]]) -> List[List[int]]:
 
         if not matrix or len(matrix) == 0 or len(matrix[0]) == 0:
@@ -74,21 +75,57 @@ class Solution:
                     queue.append([i, j])
                 else:
                     dist[i][j] = sys.maxsize
-        
+
         while queue:
             x, y = queue.popleft()
             for d in range(4):
                 next_x, next_y = x + self.dx[d], y + self.dy[d]
                 if self.isValid(next_x, next_y, matrix):
-                    if (dist[next_x][next_y] > dist[x][y] + 1):
+                    if dist[next_x][next_y] > dist[x][y] + 1:
                         dist[next_x][next_y] = dist[x][y] + 1
                         queue.append([next_x, next_y])
 
         return dist
 
-
     def isValid(self, x, y, matrix):
         return 0 <= x < len(matrix) and 0 <= y < len(matrix[0])
 
-# @lc code=end
 
+from collections import deque
+from typing import List
+
+
+class Solution:
+    def __init__(self):
+        self.dx = [1, -1, 0, 0]
+        self.dy = [0, 0, 1, -1]
+
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        m, n = len(mat), len(mat[0])
+        queue = deque()
+        res = [[float("inf")] * n for _ in range(m)]
+
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 0:
+                    queue.append((i, j))
+                    res[i][j] = 0
+
+        while queue:
+            x, y = queue.popleft()
+            for d in range(4):
+                next_x, next_y = x + self.dx[d], y + self.dy[d]
+                if (
+                    self.isValid(next_x, next_y, m, n)
+                    and res[next_x][next_y] > res[x][y] + 1
+                ):
+                    res[next_x][next_y] = res[x][y] + 1
+                    queue.append((next_x, next_y))
+
+        return res
+
+    def isValid(self, x, y, m, n):
+        return 0 <= x < m and 0 <= y < n
+
+
+# @lc code=end
