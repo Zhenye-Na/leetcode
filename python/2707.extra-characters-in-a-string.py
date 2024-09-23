7a02ca191d5fa981a5a74fd56df609c4c28b9291
Na, Zhenye <32248549+Zhenye-Na@users.noreply.github.com>
2024-09-22 21:01:30 -0700

    dp
    leetcode 092324

--- python/2707.extra-characters-in-a-string.py
+++ python/2707.extra-characters-in-a-string.py
@@ -17,47 +17,51 @@
 # have to break s into one or more non-overlapping substrings such that each
 # substring is present in dictionary. There may be some extra characters in s
 # which are not present in any of the substrings.
-# 
+#
 # Return the minimum number of extra characters left over if you break up s
 # optimally.
-# 
-# 
+#
+#
 # Example 1:
-# 
-# 
+#
+#
 # Input: s = "leetscode", dictionary = ["leet","code","leetcode"]
 # Output: 1
 # Explanation: We can break s in two substrings: "leet" from index 0 to 3 and
 # "code" from index 5 to 8. There is only 1 unused character (at index 4), so
 # we return 1.
-# 
-# 
-# 
+#
+#
+#
 # Example 2:
-# 
-# 
+#
+#
 # Input: s = "sayhelloworld", dictionary = ["hello","world"]
 # Output: 3
 # Explanation: We can break s in two substrings: "hello" from index 3 to 7 and
 # "world" from index 8 to 12. The characters at indices 0, 1, 2 are not used in
 # any substring and thus are considered as extra characters. Hence, we return
 # 3.
-# 
-# 
-# 
+#
+#
+#
 # Constraints:
-# 
-# 
+#
+#
 # 1 <= s.length <= 50
 # 1 <= dictionary.length <= 50
 # 1 <= dictionary[i].length <= 50
 # dictionary[i] and s consists of only lowercase English letters
 # dictionary contains distinct words
-# 
-# 
+#
+#
 #
 
+
 # @lc code=start
+from typing import List
+
+
 class Solution:
     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
 
@@ -69,11 +73,12 @@ class Solution:
         for i in range(1, len(s)):
             for j in range(0, i):
 
-                if s[j + 1: i + 1] in dictionary:
+                if s[j + 1 : i + 1] in dictionary:
                     dp[i] = min(dp[i], dp[j])
                 else:
                     dp[i] = min(dp[i], dp[j] + i - j)
 
         return dp[-1]
-# @lc code=end
 
+
+# @lc code=end
