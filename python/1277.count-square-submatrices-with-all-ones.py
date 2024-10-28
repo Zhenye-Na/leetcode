ea9c36d1d4d8358f7dfdf46c5cdaf3fae6cca4d2
Na, Zhenye <32248549+Zhenye-Na@users.noreply.github.com>
2024-10-28 15:34:46 -0700

    dp
    leetcode 102724

--- python/1277.count-square-submatrices-with-all-ones.py
+++ python/1277.count-square-submatrices-with-all-ones.py
@@ -15,11 +15,11 @@
 #
 # Given a m * n matrix of ones and zeros, return how many square submatrices
 # have all ones.
-# 
-# 
+#
+#
 # Example 1:
-# 
-# 
+#
+#
 # Input: matrix =
 # [
 # [0,1,1,1],
@@ -27,41 +27,45 @@
 # [0,1,1,1]
 # ]
 # Output: 15
-# Explanation: 
+# Explanation:
 # There are 10 squares of side 1.
 # There are 4 squares of side 2.
 # There is  1 square of side 3.
 # Total number of squares = 10 + 4 + 1 = 15.
-# 
-# 
+#
+#
 # Example 2:
-# 
-# 
-# Input: matrix = 
+#
+#
+# Input: matrix =
 # [
 # ⁠ [1,0,1],
 # ⁠ [1,1,0],
 # ⁠ [1,1,0]
 # ]
 # Output: 7
-# Explanation: 
-# There are 6 squares of side 1.  
-# There is 1 square of side 2. 
+# Explanation:
+# There are 6 squares of side 1.
+# There is 1 square of side 2.
 # Total number of squares = 6 + 1 = 7.
-# 
-# 
-# 
+#
+#
+#
 # Constraints:
-# 
-# 
+#
+#
 # 1 <= arr.length <= 300
 # 1 <= arr[0].length <= 300
 # 0 <= arr[i][j] <= 1
-# 
-# 
+#
+#
 #
 
+
 # @lc code=start
+from typing import List
+
+
 class Solution:
     def countSquares(self, matrix: List[List[int]]) -> int:
         if not matrix or len(matrix) == 0 or len(matrix[0]) == 0:
@@ -80,11 +84,7 @@ class Solution:
                 if i == 0 or j == 0:
                     dp[i][j] = 1
                 else:
-                    dp[i][j] = min(
-                        dp[i - 1][j - 1],
-                        dp[i][j - 1],
-                        dp[i - 1][j]
-                    ) + 1
+                    dp[i][j] = min(dp[i - 1][j - 1], dp[i][j - 1], dp[i - 1][j]) + 1
 
         count = 0
         for i in range(m):
@@ -93,5 +93,5 @@ class Solution:
 
         return count
 
-# @lc code=end
 
+# @lc code=end
